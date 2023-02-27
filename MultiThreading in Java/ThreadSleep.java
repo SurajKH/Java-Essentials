6b8d1ae897316270a013ce4b61d0d510dc3e726f
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-02-27 20:44:36 +0530

    Update ThreadSleep.java

--- MultiThreading in Java/ThreadSleep.java
+++ MultiThreading in Java/ThreadSleep.java
@@ -1,10 +1,41 @@
 package MultiThreading;
+import java.util.Scanner;
 
-public class ThreadSleep {
+class ThreadSleepClass extends Thread
+{
+	@Override
+	public void run()
+	{
+		try
+		{
+			System.out.println("Current Thread Name: "+Thread.currentThread().getName());
+			System.out.println("Current Thread ID: "+Thread.currentThread().getId());
+				//Thread sleeps for about 500 ms
+				Thread.sleep(500);
+			
+		}
+		catch(InterruptedException i)
+		{
+			System.out.println("Exception Occured..");
+			System.out.println(i);
+		}
+	}
+}
+
+public class ThreadSleep{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("                 Sleep Thread in Java MultiThreading");
+		System.out.println();
+		System.out.println("Executing the Main Thread.....");
+		ThreadSleepClass thread_1=new ThreadSleepClass();
+		ThreadSleepClass thread_2=new ThreadSleepClass();
 
+		thread_1.start();
+		thread_2.start();
+		
+		System.out.println("Main Thread Execution Completed...");
 	}
 
 }
