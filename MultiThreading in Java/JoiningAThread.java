7418adea95213149778bf82866030717bb5573d8
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-02-27 20:43:22 +0530

    Update JoiningAThread.java

--- MultiThreading in Java/JoiningAThread.java
+++ MultiThreading in Java/JoiningAThread.java
@@ -1,10 +1,43 @@
 package MultiThreading;
 
+class ThreadClassImplementation extends Thread
+{
+	public void info()
+	{
+		System.out.println("Current Thread Information: ");
+		System.out.println("Current Thread Name: "+Thread.currentThread().getName());
+		System.out.println("Current Thread ID: "+Thread.currentThread().getId());
+        
+	}
+	@Override
+	public void run()
+	{
+		//information about the thread.
+		info();
+	}
+}
 public class JoiningAThread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("             Joining a Thread in Java MultiThreading");
+		System.out.println("Current Running Thread: "+Thread.currentThread().getName());
+		ThreadClassImplementation t1=new ThreadClassImplementation();
+		ThreadClassImplementation t2=new ThreadClassImplementation();
+		t1.start();
+		//once t1 completes the task then t2 starts executing over here.
+		//join() has to placed in the try and catch block.
+		try
+		{	
+           t2.join();
+		} 
+		catch (InterruptedException e) 
+		{
+			System.out.println("Exception has Occurred.....");
+			
+		}
+		
+		System.out.println("Current Thread:"+Thread.currentThread().getName()+" got Over...");
 	}
 
 }
