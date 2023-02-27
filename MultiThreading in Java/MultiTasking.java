b4e30b7888f509f9921b60942d84a82140347687
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-02-27 20:43:57 +0530

    Update MultiTasking.java

--- MultiThreading in Java/MultiTasking.java
+++ MultiThreading in Java/MultiTasking.java
@@ -1,9 +1,37 @@
 package MultiThreading;
 
+//Multi Tasking.
+class Task1Implementation extends Thread
+{
+	@Override
+	public void run()
+	{
+		System.out.println("\nImplementation of Task 1");
+		System.out.println("Current Thread:"+Thread.currentThread().getName());
+	}
+}
+
+class Task2Implementation extends Thread
+{
+	@Override
+	public void run()
+	{
+		System.out.println("\nImplementation of Task 2");
+		System.out.println("Current Thread:"+Thread.currentThread().getName());
+	}
+}
 public class MultiTasking {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("                   MultiTasking in Java");
+		System.out.println("Current Thread: "+Thread.currentThread().getName());
+		//Creation of Threads
+		Task1Implementation task1_thread=new Task1Implementation();
+		Task2Implementation task2_thread=new Task2Implementation();
+		
+		task1_thread.start();
+		task2_thread.start();
+		
 
 	}
 
